5ca9935b0b19e3cf8c45ff8b02ba0435812edb66
Aaodon <36341331+Aaodon@users.noreply.github.com>
2018-05-23 01:44:22 +0300

    Add files via upload

---
+++ Calculator v0.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Codded by Aaodon©
+            // Data Type Variable Name = Value;
+
+            Console.Write("Enter the firt number : ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number : ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the third number : ");
+            int number3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the fourth number : ");
+            int number4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Number 1 + Number 2 + Number 3 + Number 4 = " + (number1 + number2 + number3 + number4));
+            Console.WriteLine("Number 1 - Number 2 - Number 3 - Number 4 = " + (number1 - number2 - number3 - number4));
+            Console.WriteLine("Number 1 * Number 2 * Number 3 * Number 4 = " + (number1 * number2 * number3 * number4));
+            Console.WriteLine("Number 2 / Number 2 / Number 3 / Number 4 = " + (number1 / number2 / number3 / number4));
+
+            Console.Read();
+        }
+    }
+}
